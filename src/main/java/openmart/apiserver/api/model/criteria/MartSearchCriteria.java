8dc2e48ceddec7bf7d48c78433195794942ec71c
KimSungJun <arduous1130@gmail.com>
2019-10-13 21:10:32 +0900

    API 호출시 query 스트링 decoding 처리

--- src/main/java/openmart/apiserver/api/model/criteria/MartSearchCriteria.java
+++ src/main/java/openmart/apiserver/api/model/criteria/MartSearchCriteria.java
@@ -2,9 +2,15 @@
 package openmart.apiserver.api.model.criteria;
 
 import java.io.Serializable;
+import java.io.UnsupportedEncodingException;
+import java.net.URLDecoder;
+
+import org.apache.commons.lang3.StringUtils;
 
 import lombok.Data;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Data
 public class MartSearchCriteria implements Serializable {
 	
@@ -13,5 +19,18 @@ public class MartSearchCriteria implements Serializable {
 	private String latitude; // 위도
 	private String longitude; // 경도
 	private String martName; // 마트명
+	
+	public String getMartName() {
+		
+		if (StringUtils.isNotBlank(this.martName)) {
+			try {
+				this.martName = URLDecoder.decode(this.martName, "UTF-8");
+			} catch (Exception e) {
+				log.error(e.getMessage(), e);
+			}
+		}
+		
+		return this.martName;
+	}
 }
 
