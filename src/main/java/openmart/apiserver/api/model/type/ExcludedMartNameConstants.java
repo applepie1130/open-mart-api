53e913e10a9debcc8e18f1faa0a62a90f72cd946
KimSungJun <arduous1130@gmail.com>
2019-10-16 08:49:05 +0900

    API응답결과 수정 - 제외언어 주차장 추가

--- src/main/java/openmart/apiserver/api/model/type/ExcludedMartNameConstants.java
+++ src/main/java/openmart/apiserver/api/model/type/ExcludedMartNameConstants.java
@@ -7,7 +7,7 @@ public class ExcludedMartNameConstants {
 	
 	public static final List<String> excludedMartNames = Arrays.asList(
 			"트레이더스", "이마트24", "이마트 24", "문화센터",
-			"이마트에브리데이", "이마트 에브리데이", "익스프레스"
+			"이마트에브리데이", "이마트 에브리데이", "익스프레스", "주차장"
 			);
 	
 }
