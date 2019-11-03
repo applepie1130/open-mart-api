a349eb55a4dcfd84f42566937acba01094bf0a0a
KimSungJun <arduous1130@gmail.com>
2019-11-03 21:27:18 +0900

    디폴트 휴일정보 변경 (11월)

--- src/main/java/openmart/apiserver/api/service/MartService.java
+++ src/main/java/openmart/apiserver/api/service/MartService.java
@@ -67,6 +67,7 @@ public class MartService {
 			.enable(DeserializationFeature.ACCEPT_EMPTY_STRING_AS_NULL_OBJECT)
 			.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
 	
+	private static final String FIXED_HOLIDAYS_INFO = "매월 둘째일, 넷째주 일요일 (11월10일, 11월24일)";
 	
 	/**
 	 * 위치정보를 판단하여, 주변 마트정보 조회<p>
@@ -160,7 +161,7 @@ public class MartService {
 				}
 				
 			} else if (s.getName().contains(HomeplusConstants.name) || s.getName().contains(CostcoConstants.name)){
-				holidaysInfo = "매월 둘째, 넷째 일요일 의무 휴무";
+				holidaysInfo = FIXED_HOLIDAYS_INFO;
 				
 				// JSOUP 처리
 				/*
