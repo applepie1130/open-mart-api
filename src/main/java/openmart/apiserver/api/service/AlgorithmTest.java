8f0f4d3563cc599c30aa8697a850d83bc834a7bb
SungJun Kim <arduous1130@gmail.com>
2020-04-03 17:51:49 +0900

    [#20200403]  initial commit

---
+++ src/main/java/openmart/apiserver/api/service/AlgorithmTest.java
@@ -0,0 +1,44 @@
+package openmart.apiserver.api.service;
+
+public class AlgorithmTest {
+    /**
+     *      #
+     *     ##
+     *    ###
+     *   ####
+     *  #####
+     * ######
+     * @param args
+     */
+//	public static void main(String[] args) {
+//		int n = 5;
+//		IntStream.range(0, n).forEach(t -> {
+//			IntStream.range(0, n-t-1).forEach(u -> {
+//				System.out.print(" ");
+//			});
+//			IntStream.range(n-t-1, n).forEach(u -> {
+//				System.out.print("#");
+//			});
+//			System.out.println("");
+//		});
+//	}
+
+    /**
+     * 가장 큰 수 몇개인지
+     * 4
+     * 3 2 1 3
+     * @param args
+     */
+//	public static void main(String[] args) {
+//		int[] ar = {3, 2, 1, 3};
+//		int max = Arrays.stream(ar).max().getAsInt();
+//		AtomicInteger count = new AtomicInteger();
+//		Arrays.stream(ar).forEach(t -> {
+//			if (t == max) {
+//				count.getAndIncrement();
+//			}
+//		});
+//
+//		System.out.println(count.get());
+//	}
+}
